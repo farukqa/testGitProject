d1044bebeec13eb0245b24d92b1e00b34e238d73
fhasan <fhasan@mmhayes.com>
2022-04-05 15:30:19 -0400

    Project Calculator

---
+++ src/NewCalculator.java
@@ -0,0 +1,10 @@
+public class NewCalculator {
+
+    //Richard is going to create a add method
+
+    // Craig is going to create a subtraction method
+
+    //Danielle is going to create a multiplication method
+    // Steve is going to create a division method
+}
+
