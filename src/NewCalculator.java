02a183c3b033c957b1f2332dedf70c1b2b608238
Richard Rodriguez <rrodriguez@mmhayes.com>
2022-04-05 15:46:25 -0400

    The add method is done.

--- src/NewCalculator.java
+++ src/NewCalculator.java
@@ -1,10 +1,14 @@
 public class NewCalculator {
 
     //Richard is going to create a add method
+    public static void add(double first, double second){
+        System.out.printl(first + " + " + second + " = " + (first+second) );
+    }
 
     // Craig is going to create a subtraction method
 
     //Danielle is going to create a multiplication method
+
     // Steve is going to create a division method
 }
 
